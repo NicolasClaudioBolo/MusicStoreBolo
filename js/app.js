f36325b61dbc747ef93d519a18dee6159aee8f1a
NicolasClaudioBolo <nicolascbolo@gmail.com>
2021-10-24 15:14:51 -0300

    Segundo commit

--- js/app.js
+++ js/app.js
@@ -72,31 +72,70 @@ const Saxo = new producto ("Lacquer Mayer", "Gold Alto", 400);
 
 let Instrumentos = [LesPaul, JazzBass, Telecaster, Gretsch, Steinway, Saxo];
 
-function carrito (LesPaul, JazzBass, Telecaster, Gretsch, Steinway, Saxo){
-  this.item1 = LesPaul;
-  this.item2 = JazzBass;
-  this.item3 = Telecaster;
-  this.item4 = Gretsch;
-  this.item5 = Steinway;
-  this.item6 = Saxo;
-};
+// function carrito (LesPaul, JazzBass, Telecaster, Gretsch, Steinway, Saxo){
+//   this.item1 = LesPaul;
+//   this.item2 = JazzBass;
+//   this.item3 = Telecaster;
+//   this.item4 = Gretsch;
+//   this.item5 = Steinway;
+//   this.item6 = Saxo;
+// };
 
-document.querySelector('#item1');
-document.querySelector('#item2');
-document.querySelector('#item3');
-document.querySelector('#item4');
-document.querySelector('#item5');
-document.querySelector('#item6');
+const button1 = document.querySelector('#item1');
+const button2 = document.querySelector('#item2');
+const button3 = document.querySelector('#item3');
+const button4 = document.querySelector('#item4');
+const button5 = document.querySelector('#item5');
+const button6 = document.querySelector('#item6');
 
-const inputLista = document.querySelector('#lista');
+// const Lista = document.querySelector('#lista');
 
-const listaCart = document.querySelector('#listaCart');
+const Cart = document.querySelector('#Cart');
 
-const button = document.querySelector("#btn-click");
-
-button.addEventListener('click', () => {
+button1.addEventListener('click', () => {
     const input = document.createElement('li');
-    input.textContent = inputLista.value;
-    listaCart.appendChild(document.createElement('li'));
-    console.log(inputLista.value);
+    input.textContent = (LesPaul.marca + " " + LesPaul.modelo);
+    Cart.appendChild(input);
 });
+
+button2.addEventListener('click', () => {
+  const input = document.createElement('li');
+  input.textContent = (JazzBass.marca + " " + JazzBass.modelo);
+  Cart.appendChild(input);
+});
+
+button3.addEventListener('click', () => {
+  const input = document.createElement('li');
+  input.textContent = (Telecaster.marca + " " + Telecaster.modelo);
+  Cart.appendChild(input);
+});
+
+button4.addEventListener('click', () => {
+  const input = document.createElement('li');
+  input.textContent = (Gretsch.marca + " " + Gretsch.modelo);
+  Cart.appendChild(input);
+});
+
+button5.addEventListener('click', () => {
+  const input = document.createElement('li');
+  input.textContent = (Steinway.marca + " " + Steinway.modelo);
+  Cart.appendChild(input);
+});
+
+button6.addEventListener('click', () => {
+  const input = document.createElement('li');
+  input.textContent = (Saxo.marca + " " + Saxo.modelo);
+  Cart.appendChild(input);
+});
+
+const total = document.querySelector("#total");
+
+const resultado = document.querySelector("#resultado");
+
+// total.addEventListener('click', () => {
+//   const input = document.createElement('li');
+//   input.textContent = (precio);
+//   Cart.appendChild(input);
+// });
+
+
